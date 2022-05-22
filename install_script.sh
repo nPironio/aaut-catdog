69d0a43bc6baceb4b75f70f193e326a938beb174
Carlos Giudice <carlosr.giudice@gmail.com>
2022-05-22 17:11:41 -0300

    Fixes torch installing.

--- install_script.sh
+++ install_script.sh
@@ -3,5 +3,5 @@
 pip install --upgrade pip
 
 # production
-pip install torch==1.7.0 torchvision==0.8.1 -f https://download.pytorch.org/whl/cu101/torch_stable.html --user
+pip install torch==1.7.0 torchvision==0.8.1 -f https://download.pytorch.org/whl/cu101/torch_stable.html
 pip install -r requirements.txt

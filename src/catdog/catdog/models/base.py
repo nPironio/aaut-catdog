00c8c1298e1c04cc4cb15dde41116f4f882e4bfa
Carlos Giudice <carlosr.giudice@gmail.com>
2022-06-06 21:15:34 -0300

    End to end training works :D.

--- src/catdog/catdog/models/base.py
+++ src/catdog/catdog/models/base.py
@@ -25,7 +25,9 @@ class CatDogOutput(torch.nn.Module):
 class CatDogClassifier(pl.LightningModule):
     def __init__(self, *args: Any, **kwargs: Any):
         super().__init__(*args, **kwargs)
-        self.AUROC = torchmetrics.AUROC(num_classes=2)
+        self.AUROC = torchmetrics.AUROC()  # num_classes=2 was not meant for binary problems, but multiclass problems
+        self.Precision = torchmetrics.Precision()
+        self.Recall = torchmetrics.Recall()
         self.batch_size = 32  # TODO(cgiudice): either move this to a config file or receive it as parameter
 
     def get_default_optimizer_params(self):
@@ -45,77 +47,73 @@ class CatDogClassifier(pl.LightningModule):
     def preprocess_img(self, img):
         return img
 
-    def shared_step(self, batch):
+    def _shared_step(self, batch):
         img, target, bbox = batch
         model_input = self.preprocess_img(img)
         pred_target, pred_bbox = self.model(model_input)
-        return target, pred_target, bbox, pred_bbox
-
-    def training_step(self, batch, batch_idx):
-        target, pred_target, bbox, pred_bbox = self.shared_step(batch)
-
+        
         # adds one dimension to target, just the way torch likes it
         target = target.unsqueeze(1)
-
+        
         classification_loss = F.binary_cross_entropy(pred_target, target)
         bbox_loss = F.mse_loss(pred_bbox, bbox)
+        loss = classification_loss + self.hparams.bbox_alpha * bbox_loss
+        
+        # TODO(cgiudice): maybe use self.threshold?
+        target = target.type(torch.int32)  # it is onwards assumed that only integer operations will be performed with the target
+        
+        return target, pred_target, bbox, pred_bbox, classification_loss, bbox_loss, loss
 
-        self.log('train_auroc', self.AUROC(pred_target, target.type(torch.int32)), on_step=True, on_epoch=False)
+    def training_step(self, batch, batch_idx):
+        target, pred_target, bbox, pred_bbox, classification_loss, bbox_loss, loss  = self._shared_step(batch)
+
+        self.log('train_auroc', self.AUROC(pred_target, target), on_step=True, on_epoch=False)
         self.log("train_classification_loss", classification_loss, prog_bar=True, on_step=True, on_epoch=False)
         self.log("train_bbox_loss_loss", bbox_loss, prog_bar=True, on_step=True, on_epoch=False)
 
-        return classification_loss + self.hparams.bbox_alpha * bbox_loss
+        return loss
 
-    def validation_step(self, val_batch, batch_idx):
-        target, pred_target, bbox, pred_bbox = self.shared_step(val_batch)
+    def validation_step(self, batch, batch_idx):
+        target, pred_target, bbox, pred_bbox, classification_loss, bbox_loss, loss  = self._shared_step(batch)
 
-        # adds one dimension to target, just the way torch likes it
-        target = target.unsqueeze(1)
+        self.log('val_precision', self.Precision(pred_target, target), on_step=False, on_epoch=True)    
+        self.log('vali_recall', self.Recall(pred_target, target), on_step=False, on_epoch=True)    
+        self.log('val_auroc', self.AUROC(pred_target, target), on_step=True, on_epoch=False)
+        self.log("val_classification_loss", classification_loss, prog_bar=True, on_step=True, on_epoch=False)
+        self.log("val_bbox_loss_loss", bbox_loss, prog_bar=True, on_step=True, on_epoch=False)
 
-        classification_loss = F.binary_cross_entropy(pred_target, target)
-        bbox_loss = F.mse_loss(pred_bbox, bbox)
-        loss = classification_loss + self.hparams.bbox_alpha * bbox_loss
-
-        self.log('validation_auroc', self.AUROC(pred_target, target.type(torch.int32)), on_step=True, on_epoch=False)
-        self.log("validation_classification_loss", classification_loss, prog_bar=True, on_step=True, on_epoch=False)
-        self.log("validation_bbox_loss_loss", bbox_loss, prog_bar=True, on_step=True, on_epoch=False)
-
-        imgs = val_batch[0]
+        imgs = batch[0]
         sel_idx = np.random.randint(len(imgs))
         return {"img": imgs[sel_idx], "pred_target": pred_target[sel_idx], "pred_bbox": pred_bbox[sel_idx], "loss": loss}
     
-    def test_step(self, test_batch, batch_idx):
-        # TODO(cgiudice): move common code to shared_step method
-        target, pred_target, bbox, pred_bbox = self.shared_step(test_batch)
+    def test_step(self, batch, batch_idx):
+        target, pred_target, bbox, pred_bbox, classification_loss, bbox_loss, loss  = self._shared_step(batch)
 
-        # adds one dimension to target, just the way torch likes it
-        target = target.unsqueeze(1)
+        self.log('test_precision', self.Precision(pred_target, target), on_step=False, on_epoch=True)    
+        self.log('test_recall', self.Recall(pred_target, target), on_step=False, on_epoch=True)    
+        self.log('test_auroc', self.AUROC(pred_target, target), on_step=False, on_epoch=True)    
+        self.log("test_classification_loss", classification_loss, prog_bar=True, on_step=False, on_epoch=True)    
+        self.log("test_bbox_loss", bbox_loss, prog_bar=True, on_step=False, on_epoch=True)    
 
-        classification_loss = F.binary_cross_entropy(pred_target, target)
-        bbox_loss = F.mse_loss(pred_bbox, bbox)
-        loss = classification_loss + self.hparams.bbox_alpha * bbox_loss
-
-        self.log('test_auroc', self.AUROC(pred_target, target.type(torch.int32)), on_step=True, on_epoch=False)
-        self.log("test_classification_loss", classification_loss, prog_bar=True, on_step=True, on_epoch=False)
-        self.log("test_bbox_loss_loss", bbox_loss, prog_bar=True, on_step=True, on_epoch=False)
-
-        imgs = test_batch[0]
+        imgs = batch[0]
         sel_idx = np.random.randint(len(imgs))
-        metrics_dict = {"img": imgs[sel_idx], "pred_target": pred_target[sel_idx], "pred_bbox": pred_bbox[sel_idx],
-                "loss": loss}
         
-        return metrics_dict
+        return {"img": imgs[sel_idx], "pred_target": pred_target[sel_idx], "pred_bbox": pred_bbox[sel_idx], "loss": loss}
 
     def threshold(self, y):
         return y > 0.5
 
     def validation_epoch_end(self, outputs):
-        # import ipdb; ipdb.set_trace()
-        val_loss = np.mean([x["loss"].item() for x in outputs])  # item gives us the scalar in a one element tensor
+        self._shared_epoch_end(outputs, 'val')
+
+    def test_epoch_end(self, outputs):
+        self._shared_epoch_end(outputs, 'test')
+
+    def _shared_epoch_end(self, outputs, stage):
+        loss = np.mean([x["loss"].item() for x in outputs])  # item gives us the scalar in a one element tensor
         imgs = [x["img"] for x in outputs]
         pred_bboxs = [x["pred_bbox"] for x in outputs]
         pred_targets = [x["pred_target"] for x in outputs]
-
 
         # Plot images with pred and bbox
         idxs = np.random.choice(len(imgs), size=min(3, len(imgs)), replace=False)
@@ -129,4 +127,4 @@ class CatDogClassifier(pl.LightningModule):
             figs.append(fig)
         self.logger.experiment.add_figure(tag=f"predictions sample", figure=figs, global_step=self.global_step)
 
-        self.log("val_loss", value=val_loss)
+        self.log(f"{stage}_loss", value=loss)

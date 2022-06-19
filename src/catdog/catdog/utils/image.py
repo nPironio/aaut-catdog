ff82bd3986a0c8d2b5aef3837db9516f6a132d24
nicop <nicopironio@gmail.com>
2022-06-19 20:26:45 -0300

    Random horizontal and vertical flips with bboxes

--- src/catdog/catdog/utils/image.py
+++ src/catdog/catdog/utils/image.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import matplotlib.patches as patches
+import numpy as np
+import torchvision.transforms as T
 
 
 def appropiate_padding(input_shape, kernel_shape):
@@ -20,3 +21,27 @@ def plot_image_bbox(img, category, xmin, ymin, xmax, ymax, ax=None):
     ax.add_patch(
         patches.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin, linewidth=1.5, edgecolor='r', facecolor='none'))
     ax.annotate(category, (xmax + 5, ymax + 5), color="r", size=15)
+
+
+class RandomHorizontalFlipBBox:
+    def __init__(self, p=0.5):
+        self.p = p
+        self.flipper = T.RandomHorizontalFlip(1)
+
+    def __call__(self, img, bbox: np.ndarray):
+        flip = np.random.uniform(size=len(img)) < self.p
+        img[flip] = self.flipper(img[flip])
+        bbox[flip] = [1, 0, 1, 0] - bbox[flip][:, [2, 1, 0, 3]] * [1, -1, 1, -1]
+        return img, bbox
+
+
+class RandomVerticalFlipBBox:
+    def __init__(self, p=0.5):
+        self.p = p
+        self.flipper = T.RandomVerticalFlip(1)
+
+    def __call__(self, img, bbox: np.ndarray):
+        flip = np.random.uniform(size=len(img)) < self.p
+        img[flip] = self.flipper(img[flip])
+        bbox[flip] = [0, 1, 0, 1] - bbox[flip][:, [0, 3, 2, 1]] * [-1, 1, -1, 1]
+        return img, bbox
